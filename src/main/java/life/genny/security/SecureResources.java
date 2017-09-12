7574d04f3d3146cd8db09ea68e4b2c0ac8177d48
adamcrow64 <adamcrow63@gmail.com>
2017-09-12 21:41:41 +1000

    fixed keycloak json server

--- src/main/java/life/genny/security/SecureResources.java
+++ src/main/java/life/genny/security/SecureResources.java
@@ -5,86 +5,99 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import io.vertx.core.json.DecodeException;
 import io.vertx.rxjava.core.Future;
 import io.vertx.rxjava.core.Vertx;
 
 public class SecureResources {
-	private static String keycloackFile = "./realm/localhost";
-	
-	/**
-	 * @return the keycloakJsonMap
-	 */
-	public static Map<String, String> getKeycloakJsonMap() {
-		return keycloakJsonMap;
-	}
 
-	private static Map<String, String> keycloakJsonMap = new HashMap<String, String>();
-	private static String hostIP = System.getenv("HOSTIP")!=null?System.getenv("HOSTIP"):"127.0.0.1";
+  /**
+   * @return the keycloakJsonMap
+   */
+  public static Map<String, String> getKeycloakJsonMap() {
+    return keycloakJsonMap;
+  }
 
-	/**
-	 * @param keycloakJsonMap the keycloakJsonMap to set
-	 * @return
-	 */
-	public static Future<Void> setKeycloakJsonMap(final Vertx vertx) {
-		final Future<Void> fut = Future.future();
-		vertx.executeBlocking(exec ->{
-		// Load in keycloakJsons
-		  readFilenamesFromDirectory("./realm", keycloakJsonMap);
-	// update afterwrads
-//			vertx.fileSystem().readFile(keycloackFile, d -> {
-//				if (!d.failed()) {
-//					try {
-//						String keycloakJsonText = d.result().toString().replaceAll("localhost", hostIP);
-//						keycloakJsonMap.put("localhost", keycloakJsonText);
-//						System.out.println(keycloakJsonMap);
-//						fut.complete();
-//					} catch (DecodeException dE) {
-//
-//					}
-//				} else {
-//					System.err.println("Error reading ? file!");
-//				}
-//			});
-		}, res -> {
-		});
-		return fut;
-	}
-	
-	   private static void readFilenamesFromDirectory(final String rootFilePath, final Map<String, String> keycloakJsonMap) {
-	        final File folder = new File(rootFilePath);
-	        final File[] listOfFiles = folder.listFiles();
+  private static Map<String, String> keycloakJsonMap = new HashMap<String, String>();
+  private static String hostIP =
+      System.getenv("HOSTIP") != null ? System.getenv("HOSTIP") : "127.0.0.1";
 
-	        for (int i = 0; i < listOfFiles.length; i++) {
-	            if (listOfFiles[i].isFile()) {
-	                System.out.println("File " + listOfFiles[i].getName());
-	                try {
-	                    String keycloakJsonText = getFileAsText(listOfFiles[i]);
-	                    // Handle case where dev is in place with localhost
-	                    final String localIP = System.getenv("HOSTIP");
-	                    keycloakJsonText = keycloakJsonText.replaceAll("localhost", localIP);
-	                    keycloakJsonMap.put(listOfFiles[i].getName(), keycloakJsonText);
-	                } catch (final IOException e) {
-	                    // TODO Auto-generated catch block
-	                    e.printStackTrace();
-	                }
+  /**
+   * @param keycloakJsonMap the keycloakJsonMap to set
+   * @return
+   */
+  public static Future<Void> setKeycloakJsonMap(final Vertx vertx) {
+    final Future<Void> fut = Future.future();
+    vertx.executeBlocking(exec -> {
+      // Load in keycloakJsons
+      // readFilenamesFromDirectory("./realm", keycloakJsonMap);
+      // update afterwrads
+      final List<String> filesList = vertx.fileSystem().readDirBlocking("./realm");
 
-	            } else if (listOfFiles[i].isDirectory()) {
-	                System.out.println("Directory " + listOfFiles[i].getName());
-	                readFilenamesFromDirectory(listOfFiles[i].getName(), keycloakJsonMap);
-	            }
-	        }
-	    }
+      for (final String dirFileStr : filesList) {
+        final String fileStr = new File(dirFileStr).getName();;
+        if (!"keycloak-data.json".equalsIgnoreCase(fileStr)) {
+          vertx.fileSystem().readFile(dirFileStr, d -> {
+            if (!d.failed()) {
+              try {
+                System.out.println("Loading in [" + fileStr + "]");
+                final String keycloakJsonText =
+                    d.result().toString().replaceAll("localhost", hostIP);
+                keycloakJsonMap.put(fileStr, keycloakJsonText);
+                System.out.println(keycloakJsonText);
 
-	    private static String getFileAsText(final File file) throws IOException {
-	        final BufferedReader in = new BufferedReader(new FileReader(file));
-	        String ret = "";
-	        String line = null;
-	        while ((line = in.readLine()) != null) {
-	            ret += line;
-	        }
-	        in.close();
+              } catch (final DecodeException dE) {
 
-	        return ret;
-	}
+              }
+            } else {
+              System.err.println("Error reading  file!"+fileStr);
+            }
+          });
+        }
+      }
+      fut.complete();
+    }, res -> {
+    });
+    return fut;
+  }
+
+  private static void readFilenamesFromDirectory(final String rootFilePath,
+      final Map<String, String> keycloakJsonMap) {
+    final File folder = new File(rootFilePath);
+    final File[] listOfFiles = folder.listFiles();
+
+    for (int i = 0; i < listOfFiles.length; i++) {
+      if (listOfFiles[i].isFile()) {
+        System.out.println("File " + listOfFiles[i].getName());
+        try {
+          String keycloakJsonText = getFileAsText(listOfFiles[i]);
+          // Handle case where dev is in place with localhost
+          final String localIP = System.getenv("HOSTIP");
+          keycloakJsonText = keycloakJsonText.replaceAll("localhost", localIP);
+          keycloakJsonMap.put(listOfFiles[i].getName(), keycloakJsonText);
+        } catch (final IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+
+      } else if (listOfFiles[i].isDirectory()) {
+        System.out.println("Directory " + listOfFiles[i].getName());
+        readFilenamesFromDirectory(listOfFiles[i].getName(), keycloakJsonMap);
+      }
+    }
+  }
+
+  private static String getFileAsText(final File file) throws IOException {
+    final BufferedReader in = new BufferedReader(new FileReader(file));
+    String ret = "";
+    String line = null;
+    while ((line = in.readLine()) != null) {
+      ret += line;
+    }
+    in.close();
+
+    return ret;
+  }
 }

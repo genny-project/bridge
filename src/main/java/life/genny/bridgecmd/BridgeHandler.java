36b4fb918951584e43bf0b26b84378dbcded8d3d
adamcrow64 <adamcrow63@gmail.com>
2017-09-15 08:37:27 +1000

    Updated token

--- src/main/java/life/genny/bridgecmd/BridgeHandler.java
+++ src/main/java/life/genny/bridgecmd/BridgeHandler.java
@@ -1,30 +1,31 @@
 package life.genny.bridgecmd;
 
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.handler.sockjs.BridgeEventType;
 import io.vertx.rxjava.core.Vertx;
 import io.vertx.rxjava.core.eventbus.MessageProducer;
 import io.vertx.rxjava.ext.web.handler.sockjs.BridgeEvent;
 import io.vertx.rxjava.ext.web.handler.sockjs.SockJSHandler;
 import life.genny.channels.EBProducers;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
 public class BridgeHandler {
 
 	private static final Logger logger = LoggerFactory.getLogger(ServiceVerticle.class);
 
-	protected static SockJSHandler eventBusHandler(Vertx vertx) {	
-		MessageProducer<JsonObject> toAddressOutbound = vertx.eventBus().publisher("address.outbound");
+	protected static SockJSHandler eventBusHandler(final Vertx vertx) {
+		final MessageProducer<JsonObject> toAddressOutbound = vertx.eventBus().publisher("address.outbound");
 		EBProducers.setToClientOutbound(toAddressOutbound);
-		SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
+		final SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
 		return sockJSHandler.bridge(BridgeConfig.setBridgeOptions(), BridgeHandler::bridgeHandler);
 	}
 
-	protected static void bridgeHandler(BridgeEvent bridgeEvent) {
+	protected static void bridgeHandler(final BridgeEvent bridgeEvent) {
 		if (bridgeEvent.type() == BridgeEventType.PUBLISH || bridgeEvent.type() == BridgeEventType.SEND) {
 			JsonObject rawMessage = bridgeEvent.getRawMessage().getJsonObject("body");
-			String token = bridgeEvent.getRawMessage().getString("token");
+			final String token = bridgeEvent.getRawMessage().getString("token");
 			rawMessage = rawMessage.getJsonObject("data");
+			rawMessage.put("token", token);
 			logger.info("Incoming Frontend Event :" + rawMessage);
 			logger.info("PUBLISH to events...");
 			EBProducers.getToEvents().write(rawMessage);

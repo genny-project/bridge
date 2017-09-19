cf1628660aaacfa208b6b37464de6430cb2e8647
adamcrow64 <adamcrow63@gmail.com>
2017-09-19 22:58:48 +1000

    Added logback file to stop annoying netty logs

--- src/main/java/life/genny/bridgecmd/ServiceVerticle.java
+++ src/main/java/life/genny/bridgecmd/ServiceVerticle.java
@@ -1,10 +1,8 @@
 package life.genny.bridgecmd;
 
-import io.vertx.core.http.HttpMethod;
+
 import io.vertx.rxjava.core.AbstractVerticle;
 import io.vertx.rxjava.core.Future;
-import io.vertx.rxjava.core.Vertx;
-import io.vertx.rxjava.ext.web.Router;
 import life.genny.cluster.Cluster;
 import life.genny.security.SecureResources;
 
@@ -12,15 +10,17 @@ public class ServiceVerticle extends AbstractVerticle {
 
 	@Override
 	public void start() {
+	  
+	  
 		System.out.println("Setting up routes");
-		Future<Void> startFuture = Future.future();
+		final Future<Void> startFuture = Future.future();
 		Cluster.joinCluster(vertx).compose( res -> {
-			Future<Void> fut = Future.future();
+			final Future<Void> fut = Future.future();
 			SecureResources.setKeycloakJsonMap(vertx).compose(p -> {
 				Routers.routers(vertx);
 				fut.complete();
 			},fut);
 			startFuture.complete();
 		},startFuture);
-	}	
+	}
 }

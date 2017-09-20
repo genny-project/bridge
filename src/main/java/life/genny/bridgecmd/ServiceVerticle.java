f12efaf639b9465757b090d3e68caa0586cf494a
adamcrow64 <adamcrow63@gmail.com>
2017-09-20 11:09:01 +1000

    Started checkstyle fixes

--- src/main/java/life/genny/bridgecmd/ServiceVerticle.java
+++ src/main/java/life/genny/bridgecmd/ServiceVerticle.java
@@ -8,19 +8,17 @@ import life.genny.security.SecureResources;
 
 public class ServiceVerticle extends AbstractVerticle {
 
-	@Override
-	public void start() {
-	  
-	  
-		System.out.println("Setting up routes");
-		final Future<Void> startFuture = Future.future();
-		Cluster.joinCluster(vertx).compose( res -> {
-			final Future<Void> fut = Future.future();
-			SecureResources.setKeycloakJsonMap(vertx).compose(p -> {
-				Routers.routers(vertx);
-				fut.complete();
-			},fut);
-			startFuture.complete();
-		},startFuture);
-	}
+  @Override
+  public void start() {
+    System.out.println("Setting up routes");
+    final Future<Void> startFuture = Future.future();
+    Cluster.joinCluster(vertx).compose(res -> {
+      final Future<Void> fut = Future.future();
+      SecureResources.setKeycloakJsonMap(vertx).compose(p -> {
+        Routers.routers(vertx);
+        fut.complete();
+      }, fut);
+      startFuture.complete();
+    }, startFuture);
+  }
 }

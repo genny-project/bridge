87d07079a3253979cde40855baad4fe01b18209b
andres <helios@andress-MBP.lan>
2017-12-15 10:25:59 +1100

    uncommented code for handling api

--- src/main/java/life/genny/bridgecmd/Routers.java
+++ src/main/java/life/genny/bridgecmd/Routers.java
@@ -13,13 +13,14 @@ public class Routers {
     router.route().handler(RouterHandlers.cors());
     router.route("/frontend/*").handler(BridgeHandler.eventBusHandler(vertx));
     router.route(HttpMethod.GET, "/api/events/init").handler(RouterHandlers::apiGetInitHandler);
-//    router.route(HttpMethod.POST, "/api/events/init").handler(RouterHandlers::apiInitHandler);
+    router.route(HttpMethod.POST, "/api/events/init").handler(RouterHandlers::apiInitHandler);
     router.route(HttpMethod.GET, "/api/session").handler(RouterHandlers::apiSession);
     router.route(HttpMethod.POST, "/api/service").handler(RouterHandlers::apiServiceHandler);
     router.route(HttpMethod.POST, "/api/cmds").handler(RouterHandlers::apiHandler);
     router.route(HttpMethod.POST, "/api/data").handler(RouterHandlers::apiHandler);
     router.route(HttpMethod.GET, "/version").handler(VersionHandler::apiGetVersionHandler);
 
+    
     vertx.createHttpServer().requestHandler(router::accept).listen(serverPort);
   }
   

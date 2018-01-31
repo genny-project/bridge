ca9e988067995d97619edbeef0c8f8077189f2b9
Adam Crow <acrow@Adams-Compooter.local>
2018-01-31 22:53:05 +1100

    Upgraded to qwanda 0.9.22 and gson consolidation

--- src/main/java/life/genny/bridge/RouterHandlers.java
+++ src/main/java/life/genny/bridge/RouterHandlers.java
@@ -57,20 +57,7 @@ public class RouterHandlers {
 				.allowedHeader("X-Requested-With");
 	}
 	
-	static Gson gson = new GsonBuilder()
-			.registerTypeAdapter(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
-				@Override
-				public LocalDateTime deserialize(final JsonElement json, final Type type,
-						final JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-					return LocalDateTime.parse(json.getAsJsonPrimitive().getAsString(),
-							DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-				}
 
-				public JsonElement serialize(final LocalDateTime date, final Type typeOfSrc,
-						final JsonSerializationContext context) {
-					return new JsonPrimitive(date.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)); // "yyyy-mm-dd"
-				}
-			}).create();
 
 
 	public static void apiGetInitHandler(final RoutingContext routingContext) {

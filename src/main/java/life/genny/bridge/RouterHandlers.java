648290a192bf9dcae1a441ae4dd606ec463a6a7b
Adam Crow <acrow@Adams-Compooter.local>
2018-03-14 11:44:09 +1100

    Added api access to brisge cache

--- src/main/java/life/genny/bridge/RouterHandlers.java
+++ src/main/java/life/genny/bridge/RouterHandlers.java
@@ -181,45 +181,53 @@ public class RouterHandlers {
 		routingContext.response().end();
 	}
 
-//	public static void apiMapPutHandler(final RoutingContext context) {
-//		ClusterMap.mapDTT(context);
-//	}
 
-	public static void apiMapGetHandler(final RoutingContext context) {
-		final HttpServerRequest req = context.request();
-		String param1 = req.getParam("param1");
+	 public static void apiMapPutHandler(final RoutingContext context) {
+		    
+		    
+		    //handle the body here and assign it to wifiPayload to process the data 
+		    final HttpServerRequest req = context.request().bodyHandler(boddy -> {
+		   //   System.out.println(boddy.toJsonObject());
+		    	  JsonObject wifiPayload = boddy.toJsonObject();
+		      if (wifiPayload == null) {
+		    	  context.request().response().headers().set("Content-Type", "application/json");
+		          JsonObject err = new JsonObject().put("status", "error");
+		          context.request().response().headers().set("Content-Type", "application/json");
+		          context.request().response().end(err.encode());
+		        } 
+		      else {
+		          // a JsonObject wraps a map and it exposes type-aware getters
+		          String param1 = wifiPayload.getString("key");
+		          System.out.println("CACHE KEY:"+param1);
+		          String param2 = wifiPayload.getString("json");
+		          VertxUtils.writeCachedJson(param1, param2);
+		         
+		                  JsonObject ret = new JsonObject().put("status", "ok");
+		                  context.request().response().headers().set("Content-Type", "application/json");
+		                  context.request().response().end(ret.encode());
 
-		SharedData sd = Vertx.currentContext().owner().sharedData();
+		        }
+		    });
 
-		if (System.getenv("GENNY_DEV") == null) {
+		    
+		    
 
-			sd.getClusterWideMap("shared_data", (AsyncResult<AsyncMap<String, String>> res) -> {
-				if (res.failed()) {
-					JsonObject err = new JsonObject().put("status", "error");
-					req.response().headers().set("Content-Type", "application/json");
-					req.response().end(err.encode());
-				} else {
-					AsyncMap<String, String> amap = res.result();
-					amap.get(param1, (AsyncResult<String> comp) -> {
-						if (comp.failed()) {
-							JsonObject err = new JsonObject().put("status", "error").put("description", "write failed");
-							req.response().headers().set("Content-Type", "application/json");
-							req.response().end(err.encode());
-						} else {
-							JsonObject err = new JsonObject().put("status", "ok").put("value", comp.result());
-							req.response().headers().set("Content-Type", "application/json");
-							req.response().end(err.encode());
-						}
-					});
-				}
-			});
-		} else {
-			String result = (String) sd.getLocalMap("shared_data").get(param1);
-			JsonObject err = new JsonObject().put("status", "ok").put("value", result);
-			req.response().headers().set("Content-Type", "application/json");
-			req.response().end(err.encode());
-		}
+		  }
 
-	}
+		  public static void apiMapGetHandler(final RoutingContext context) {
+		    final HttpServerRequest req = context.request();
+		    String param1 = req.getParam("param1");
+
+		    JsonObject json = VertxUtils.readCachedJson(param1);
+		      if (json.getString("status").equals("error")) {
+		        JsonObject err = new JsonObject().put("status", "error");
+		        req.response().headers().set("Content-Type", "application/json");
+		        req.response().end(err.encode());
+		      } else {
+		            req.response().headers().set("Content-Type", "application/json");
+		            req.response().end(json.encode());
+		          }
+
+		  }
 
 }

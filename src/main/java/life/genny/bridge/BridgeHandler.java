48bf6ffb008d76a95e5b1eba79cc5ca1146ed330
andres <helios@andress-MacBook-Pro-2.local>
2018-04-06 05:22:25 +1000

    fixed bug in MessageProducer

--- src/main/java/life/genny/bridge/BridgeHandler.java
+++ src/main/java/life/genny/bridge/BridgeHandler.java
@@ -12,6 +12,7 @@ import io.vertx.rxjava.core.streams.ReadStream;
 import io.vertx.rxjava.ext.web.handler.sockjs.BridgeEvent;
 import io.vertx.rxjava.ext.web.handler.sockjs.SockJSHandler;
 import life.genny.channel.Producer;
+import life.genny.cluster.CurrentVtxCtx;
 
 public class BridgeHandler {
 
@@ -23,8 +24,9 @@ public class BridgeHandler {
     return sockJSHandler.bridge(BridgeConfig.setBridgeOptions(), BridgeHandler::bridgeHandler);
 
   }
+
   public static JsonObject msgTmp = null;
-  
+
   protected static void bridgeHandler(final BridgeEvent bridgeEvent) {
     if (bridgeEvent.type() == BridgeEventType.PUBLISH
         || bridgeEvent.type() == BridgeEventType.SEND) {
@@ -37,11 +39,14 @@ public class BridgeHandler {
           if (Producer.getToData().writeQueueFull()) {
             log.error(
                 "WEBSOCKET EVNT >> PRODUCER GETTODATA IS FULL: #####################################");
-            msgTmp = rawMessage;
-            Producer.getToData().drainHandler(handler -> {
-              Producer.getToData().send(msgTmp);
-            });
-            msgTmp.clear();
+            // msgTmp = rawMessage;
+            Producer.setToData(
+                CurrentVtxCtx.getCurrentCtx().getClusterVtx().eventBus().publisher("data"));
+            // Producer.getToData().drainHandler(handler -> {
+            // Producer.getToData().send(msgTmp);
+            // });
+            // msgTmp.clear();
+            Producer.getToData().send(rawMessage).end();
           } else {
             log.info("WEBSOCKET EVNT >> PRODUCER GETTODATA NOT FULL:");
             Producer.getToData().send(rawMessage).end();
@@ -51,7 +56,6 @@ public class BridgeHandler {
             // Pump.pump(hlr.result().rxReply("got it").toObservable(), null).start();
             // });
           }
-
         } else if (rawMessage.getString("msg_type").equals("EVT_MSG")) {
           log.info("WEBSOCKET EVNT >> EVENT-BUS EVNT:" + rawMessage.getString("event_type") + ":"
               + rawMessage.getJsonObject("data").getString("code") + ":"
@@ -59,11 +63,14 @@ public class BridgeHandler {
           if (Producer.getToEvents().writeQueueFull()) {
             log.error(
                 "WEBSOCKET EVNT >> PRODUCER GETTOEVENTS IS FULL: ###################################");
-            msgTmp = rawMessage;
-            Producer.getToEvents().drainHandler(handler -> {
-              Producer.getToEvents().send(msgTmp);
-            });
-            msgTmp.clear();
+            Producer.setToEvents(
+                CurrentVtxCtx.getCurrentCtx().getClusterVtx().eventBus().publisher("events"));
+            // msgTmp = rawMessage;
+            // Producer.getToEvents().drainHandler(handler -> {
+            // Producer.getToEvents().send(msgTmp);
+            // });
+            // msgTmp.clear();
+            Producer.getToEvents().send(rawMessage).end();
           } else {
             log.info("WEBSOCKET EVNT >> PRODUCER GETTOEVENTS NOT FULL:");
             Producer.getToEvents().send(rawMessage).end();

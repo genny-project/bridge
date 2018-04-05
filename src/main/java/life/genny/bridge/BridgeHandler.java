8cfbdcffe6d8f128704eadd6d4c0ce8ca66c931b
andres <helios@andress-MacBook-Pro-2.local>
2018-04-06 00:25:23 +1000

    added logs to when Event Bus is full

--- src/main/java/life/genny/bridge/BridgeHandler.java
+++ src/main/java/life/genny/bridge/BridgeHandler.java
@@ -1,10 +1,8 @@
 package life.genny.bridge;
 
 import java.lang.invoke.MethodHandles;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.Logger;
-
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.bridge.BridgeEventType;
 import io.vertx.rxjava.core.Vertx;
@@ -14,34 +12,48 @@ import life.genny.channel.Producer;
 
 public class BridgeHandler {
 
-	protected static final Logger log = org.apache.logging.log4j.LogManager
-			.getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
+  protected static final Logger log = org.apache.logging.log4j.LogManager
+      .getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
 
-	protected static SockJSHandler eventBusHandler(final Vertx vertx) {
-		final SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
-		return sockJSHandler.bridge(BridgeConfig.setBridgeOptions(), BridgeHandler::bridgeHandler);
+  protected static SockJSHandler eventBusHandler(final Vertx vertx) {
+    final SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
+    return sockJSHandler.bridge(BridgeConfig.setBridgeOptions(), BridgeHandler::bridgeHandler);
 
-	}
+  }
 
-	protected static void bridgeHandler(final BridgeEvent bridgeEvent) {
-		if (bridgeEvent.type() == BridgeEventType.PUBLISH || bridgeEvent.type() == BridgeEventType.SEND) {
-			JsonObject rawMessage = bridgeEvent.getRawMessage().getJsonObject("body");
-			rawMessage = rawMessage.getJsonObject("data");
-			if (rawMessage.getString("token") != null) { // do not allow empty tokens
-				if (rawMessage.getString("msg_type").equals("DATA_MSG")) {
-					log.info("WEBSOCKET DATA >> EVENT-BUS DATA:" + rawMessage.getString("data_type") + ":"
-							+ StringUtils.abbreviateMiddle(rawMessage.getString("token"), "...", 40));
-					Producer.getToData().send(rawMessage);
-				} else if (rawMessage.getString("msg_type").equals("EVT_MSG")) {
-					log.info("WEBSOCKET EVNT >> EVENT-BUS EVNT:" + rawMessage.getString("event_type") + ":"
-							+ rawMessage.getJsonObject("data").getString("code") + ":"
-							+ StringUtils.abbreviateMiddle(rawMessage.getString("token"), "...", 40));
-					Producer.getToEvents().send(rawMessage);
-				}
-			} else {
-				System.out.println("EMPTY TOKEN");
-			}
-		}
-		bridgeEvent.complete(true);
-	}
+  protected static void bridgeHandler(final BridgeEvent bridgeEvent) {
+    if (bridgeEvent.type() == BridgeEventType.PUBLISH
+        || bridgeEvent.type() == BridgeEventType.SEND) {
+      JsonObject rawMessage = bridgeEvent.getRawMessage().getJsonObject("body");
+      rawMessage = rawMessage.getJsonObject("data");
+      if (rawMessage.getString("token") != null) { // do not allow empty tokens
+        if (rawMessage.getString("msg_type").equals("DATA_MSG")) {
+          log.info("WEBSOCKET DATA >> EVENT-BUS DATA:" + rawMessage.getString("data_type") + ":"
+              + StringUtils.abbreviateMiddle(rawMessage.getString("token"), "...", 40));
+          if (Producer.getToData().writeQueueFull()) {
+            log.error("WEBSOCKET EVNT >> PRODUCER GETTODATA IS FULL: #####################################");
+          } else {
+            log.info("WEBSOCKET EVNT >> PRODUCER GETTODATA NOT FULL:");
+            Producer.getToData().send(rawMessage);
+          }
+          
+        } else if (rawMessage.getString("msg_type").equals("EVT_MSG")) {
+
+          log.info("WEBSOCKET EVNT >> EVENT-BUS EVNT:" + rawMessage.getString("event_type") + ":"
+              + rawMessage.getJsonObject("data").getString("code") + ":"
+              + StringUtils.abbreviateMiddle(rawMessage.getString("token"), "...", 40));
+          if (Producer.getToEvents().writeQueueFull()) {
+            log.error("WEBSOCKET EVNT >> PRODUCER GETTOEVENTS IS FULL: ###################################");
+          } else {
+            log.info("WEBSOCKET EVNT >> PRODUCER GETTOEVENTS NOT FULL:");
+            Producer.getToEvents().send(rawMessage);
+          }
+          
+        }
+      } else {
+        System.out.println("EMPTY TOKEN");
+      }
+    }
+    bridgeEvent.complete(true);
+  }
 }

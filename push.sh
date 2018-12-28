752bfec100f7bba3c7fb709bc407ad56ea052bf6
Adam Crow <acrow@Phantom.local>
2018-12-28 17:22:05 +1100

    Updated push.sh

--- push.sh
+++ push.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+project=bridge
+file="src/main/resources/bridge-git.properties"
+
+function prop {
+    grep "${1}=" ${file}|cut -d'=' -f2
+}
 
 if [ -z "${1}" ]; then
    version="latest"
@@ -6,8 +12,17 @@ else
    version="${1}"
 fi
 
+if [ -f "$file" ]
+then
+  echo "$file found."
 
-docker push gennyproject/bridge:"${version}"
-docker tag  gennyproject/bridge:"${version}"  gennyproject/bridge:latest
-docker push gennyproject/bridge:latest
+  echo "git.commit.id = " $(prop 'git.commit.id')
+  echo "git.build.version = " $(prop 'git.build.version')
+  docker push gennyproject/${project}:latest
+  docker push gennyproject/${project}:"${version}"
+  docker push gennyproject/${project}:$(prop 'git.commit.id')
+  docker push gennyproject/${project}:$(prop 'git.build.version')
+else
+  echo "ERROR: git properties $file not found."
+fi
 

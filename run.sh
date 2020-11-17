3178b02a1e90e7fe1f82f5971c56687d9ebedfb5
Byron Andres <heliosphanes@outlook.com>
2020-11-17 15:46:36 +1100

    replace service verticle  with a postcontruct, add the jar and the target folder to the image, add quarkus scripts and scripts to run bridge plust the cluster.xml and application.properties

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+MYIP=''
+GetIP()
+{
+   MYIP=
+   while IFS=$': \t' read -a line ;do
+       [ -z "${line%inet}" ] && ip=${line[${#line[1]}>4?1:2]} &&
+           [ "${ip#127.0.0.1}" ] && MYIP=$ip
+     done< <(LANG=C /sbin/ifconfig $1)
+}
+
+CheckInterface(){
+   ifconfig $1 > /dev/null 2>&1
+}
+
+CheckInterface eth0 && GetIP eth0 || CheckInterface en0  && GetIP en0
+
+export MYIP=$MYIP
+
+ADMIN_PASSWORD=GENNY
+ADMIN_USERNAME=GENNY
+
+java -jar -Dadmin.username=${ADMIN_USERNAME} -Dadmin.password=${ADMIN_PASSWORD} target/bridge-7.6.0-runner.jar

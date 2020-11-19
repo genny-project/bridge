7bc94c92e358e8c28fd491c296bcf016510fadd2
Adam Crow <adamcrow63@gmail.com>
2020-11-19 12:48:53 +1100

    Updated

--- build-docker.sh
+++ build-docker.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
-
-project=bridge
+project=${PWD##*/}
 file="src/main/resources/${project}-git.properties"
-
+org=gennyproject
 function prop() {
   grep "${1}=" ${file} | cut -d'=' -f2
 }
+version=$(prop 'git.build.version')
 
-if [ -z "${1}" ]; then
-  version="latest"
-else
-  version="${1}"
-fi
 
-if [ -f "$file" ]; then
-  echo "$file found."
-  echo "git.commit.id = " "$(prop 'git.commit.id')"
-  echo "git.build.version = " "$(prop 'git.build.version')"
-  docker build -t gennyproject/${project}:"${version}" .
-else
-  echo "ERROR: git properties $file not found."
-fi
-
-image_ids=$(docker images | grep ${project} | grep none)
-if [ "${image_ids:-0}" == 0 ]; then
-  echo 'Skip clean up'
-else
-  docker images | grep ${project} | grep none | awk '{print $3}' | xargs docker rmi
-fi
+./mvnw clean package -Dquarkus.container-image.build=true -DskipTests=true
+docker tag ${USER}/${project}:${version} ${org}/${project}:${version}
+docker tag ${USER}/${project}:${version} ${org}/${project}:latest

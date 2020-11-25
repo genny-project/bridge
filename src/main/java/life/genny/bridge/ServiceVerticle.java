5a461ff9c9416122834a160e04c469bfbfebc340
Byron Andres <heliosphanes@outlook.com>
2020-11-25 16:43:50 +1100

    add quarkus functionality

--- src/main/java/life/genny/bridge/ServiceVerticle.java
+++ src/main/java/life/genny/bridge/ServiceVerticle.java
@@ -2,39 +2,70 @@ package life.genny.bridge;
 
 
 import java.lang.invoke.MethodHandles;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
 import org.apache.logging.log4j.Logger;
-import io.vertx.core.AbstractVerticle;
+
+import io.quarkus.runtime.Startup;
 import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
+import life.genny.channel.Consumer;
+import life.genny.channel.Producer;
 import life.genny.channel.Routers;
 import life.genny.cluster.Cluster;
+import life.genny.cluster.CurrentVtxCtx;
 import life.genny.eventbus.EventBusInterface;
 import life.genny.eventbus.EventBusVertx;
 import life.genny.eventbus.VertxCache;
 import life.genny.qwandautils.GennyCacheInterface;
 import life.genny.utils.VertxUtils;
 
-public class ServiceVerticle extends AbstractVerticle {
+@Startup
+@ApplicationScoped
+public class ServiceVerticle {
 	
-	  protected static final Logger log = org.apache.logging.log4j.LogManager
-		      .getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
+  protected static final Logger log = org.apache.logging.log4j.LogManager.getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
 
 
-  @Override
-  public void start() {
-    log.info("Setting up routes");
-    final Future<Void> startFuture = Future.future();
-    Cluster.joinCluster().compose(res -> {
-      EventBusInterface eventBus = new EventBusVertx();
-      GennyCacheInterface vertxCache = new VertxCache();
-      VertxUtils.init(eventBus,vertxCache);
-      Routers.routers(vertx);
-      BridgeRouters.routers(vertx);
-      Routers.activate(vertx);
-      log.info("Bridge now ready");
-      
-      EBCHandlers.registerHandlers();
-      startFuture.complete();
-    }, startFuture);
-    
-  }
+  @Inject 
+  Vertx vertx;
+//  public void start() {
+//    log.info("Setting up routes");
+//    final Future<Void> startFuture = Future.future();
+//    Cluster.joinCluster().compose(res -> {
+//      EventBusInterface eventBus = new EventBusVertx();
+//      GennyCacheInterface vertxCache = new VertxCache();
+//      VertxUtils.init(eventBus,vertxCache);
+//      Routers.routers(vertx);
+//      BridgeRouters.routers(vertx);
+//      Routers.activate(vertx);
+//      log.info("Bridge now ready");
+//      
+//      EBCHandlers.registerHandlers();
+//      startFuture.complete();
+//    }, startFuture);
+//    
+//  }
+   @PostConstruct
+     public void start() {
+       EventBus eb = vertx.eventBus();
+       Cluster.joinCluster();
+       System.out.println(vertx.isClustered());
+       Consumer.registerAllConsumer(eb);
+       Producer.registerAllProducers(eb);
+         CurrentVtxCtx.getCurrentCtx().setClusterVtx(vertx);
+         EventBusInterface eventBus = new EventBusVertx();
+         GennyCacheInterface vertxCache = new VertxCache();
+         VertxUtils.init(eventBus,vertxCache);
+         Routers.routers(vertx);
+         BridgeRouters.routers(vertx);
+         Routers.activate(vertx);
+         
+         EBCHandlers.registerHandlers();
+       
+     }
 }

915e910b4a21ce9d1c809561eb3d20aa770c1aa5
Your Name <you@example.com>
2021-04-15 03:41:22 +0000

    add handler exceptions for virtualbus

--- src/main/java/life/genny/bridge/VirtualChannelServices.java
+++ src/main/java/life/genny/bridge/VirtualChannelServices.java
@@ -1,5 +1,6 @@
 package life.genny.bridge;
 
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import io.vertx.core.json.JsonObject;
@@ -9,8 +10,11 @@ import javax.ws.rs.Path;
 
 @Path("/eventbus")
 @RegisterRestClient
+@RegisterProvider(value = MyMapper.class,
+                  priority = 50)
 public interface VirtualChannelServices {
 
     @POST
+    @Path("/datawithreply")
     JsonObject sendPayload(Object name);
 }

06328ec4080a6c4502330cb48f75a1073306913f
cyberust <you@example.com>
2021-06-08 09:13:39 +0000

    add quarkus endpoint to fix sync endpoint

--- src/main/java/life/genny/bridge/BridgeRouters.java
+++ src/main/java/life/genny/bridge/BridgeRouters.java
@@ -20,57 +20,51 @@ import life.genny.qwandautils.GennySettings;
 
 @ApplicationScoped
 public class BridgeRouters {
-	  protected static final Logger log = org.apache.logging.log4j.LogManager
-		      .getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
 
-		@Inject
-		BridgeRouterHandlers bridgeRouterHandlers;
+	protected static final Logger log = org.apache.logging.log4j.LogManager
+		.getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
 
-		@Inject
-		BridgeHandler bridgeHandler;
-  
+	@Inject
+	BridgeRouterHandlers bridgeRouterHandlers;
+
+	@Inject
+	BridgeHandler bridgeHandler;
+
 	static private Router bridgeRouter = null;
- 
-  //protected static void routers(final Vertx vertx) {
-  protected void routers(final Vertx vertx) {
-	  bridgeRouter = Router.router(vertx);  // create new router
-	  
-	  BridgeRouterHandlers.avertx = vertx;
 
-	  bridgeRouter.route().handler(BridgeRouterHandlers.cors());
+	protected void routers(final Vertx vertx) {
+		bridgeRouter = Router.router(vertx);  // create new router
 
-	  bridgeRouter.route("/frontend/*").subRouter(bridgeHandler.eventBusHandler(vertx));
-	  bridgeRouter.route(HttpMethod.GET, "/api/events/init").handler(BridgeRouterHandlers::apiGetInitHandler);
-	  bridgeRouter.route(HttpMethod.POST, "/api/events/init").handler(BridgeRouterHandlers::apiInitHandler);
-	  bridgeRouter.route(HttpMethod.POST, "/api/service").handler(bridgeRouterHandlers::apiServiceHandler);
-	  bridgeRouter.route(HttpMethod.POST, "/api/service/sync").handler(bridgeRouterHandlers::apiSyncHandler).handler(TimeoutHandler.create(120000));   // old mobile
-	  bridgeRouter.route(HttpMethod.POST, "/v7/api/service/sync").blockingHandler(bridgeRouterHandlers::apiSync2Handler).handler(TimeoutHandler.create(120000)); // mobile v7
+		BridgeRouterHandlers.avertx = vertx;
 
-	  bridgeRouter.route(HttpMethod.GET, "/api/pull/:key").handler(BridgeRouterHandlers::apiGetPullHandler);
+		bridgeRouter.route().handler(BridgeRouterHandlers.cors());
 
-	  bridgeRouter.route(HttpMethod.GET, "/version").handler(BridgeRouterHandlers::apiGetVersionHandler);
-      bridgeRouter.route(HttpMethod.GET, "/health").handler(BridgeRouterHandlers::apiGetHealthHandler);
+		bridgeRouter.route("/frontend/*").subRouter(bridgeHandler.eventBusHandler(vertx));
+		bridgeRouter.route(HttpMethod.GET, "/api/events/init").handler(BridgeRouterHandlers::apiGetInitHandler);
+		bridgeRouter.route(HttpMethod.POST, "/api/events/init").handler(BridgeRouterHandlers::apiInitHandler);
+		bridgeRouter.route(HttpMethod.POST, "/api/service").handler(bridgeRouterHandlers::apiServiceHandler);
+		bridgeRouter.route(HttpMethod.POST, "/api/service/sync").handler(bridgeRouterHandlers::apiSyncHandler).handler(TimeoutHandler.create(120000));   // old mobile
+		bridgeRouter.route(HttpMethod.POST, "/v7/api/service/sync").blockingHandler(bridgeRouterHandlers::apiSync2Handler).handler(TimeoutHandler.create(120000)); // mobile v7
 
-	  bridgeRouter.route(HttpMethod.POST, "/api/cmds").handler(bridgeRouterHandlers::apiHandler);
-	  bridgeRouter.route(HttpMethod.POST, "/api/data").handler(bridgeRouterHandlers::apiHandler);
-	  bridgeRouter.route(HttpMethod.POST, "/api/devices").blockingHandler(bridgeRouterHandlers::apiDevicesHandler);
-	  bridgeRouter.route(HttpMethod.GET, "/api/search").handler(BridgeRouterHandlers::apiSearchHandler);
+		bridgeRouter.route(HttpMethod.GET, "/api/pull/:key").handler(BridgeRouterHandlers::apiGetPullHandler);
 
-	  bridgeRouter.route(HttpMethod.POST, "/api/virtualbus").blockingHandler(bridgeRouterHandlers::virtualEventBusHandler);
-    
-	  //bridgeRouter.route(HttpMethod.GET, "/metrics").handler(Metrics::metrics);
-	log.info("Activating Bridge Routes on port "+GennySettings.apiPort+" given ["+GennySettings.apiPort+"]");
-	
-	HttpServerOptions serverOptions = new HttpServerOptions();
-	//  serverOptions.setUsePooledBuffers(true);
-	//  serverOptions.setCompressionSupported(true);
-	//  serverOptions.setCompressionLevel(3);
+		bridgeRouter.route(HttpMethod.GET, "/version").handler(BridgeRouterHandlers::apiGetVersionHandler);
+		bridgeRouter.route(HttpMethod.GET, "/health").handler(BridgeRouterHandlers::apiGetHealthHandler);
 
-	//  serverOptions.setUseAlpn(true);
-	
-	
-	vertx.createHttpServer(serverOptions).requestHandler(bridgeRouter::accept).listen(Integer.parseInt(GennySettings.apiPort));
+		bridgeRouter.route(HttpMethod.POST, "/api/cmds").handler(bridgeRouterHandlers::apiHandler);
+		bridgeRouter.route(HttpMethod.POST, "/api/data").handler(bridgeRouterHandlers::apiHandler);
+		bridgeRouter.route(HttpMethod.POST, "/api/devices").blockingHandler(bridgeRouterHandlers::apiDevicesHandler);
+		bridgeRouter.route(HttpMethod.GET, "/api/search").handler(BridgeRouterHandlers::apiSearchHandler);
 
-  }
+		bridgeRouter.route(HttpMethod.POST, "/api/virtualbus").blockingHandler(bridgeRouterHandlers::virtualEventBusHandler);
+
+		log.info("Activating Bridge Routes on port "+GennySettings.apiPort+" given ["+GennySettings.apiPort+"]");
+
+		HttpServerOptions serverOptions = new HttpServerOptions();
+
+
+		vertx.createHttpServer(serverOptions).requestHandler(bridgeRouter::accept).listen(Integer.parseInt(GennySettings.apiPort));
+
+	}
 
 }
